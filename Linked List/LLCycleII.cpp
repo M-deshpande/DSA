a3695a5edefa7b2dfda55787f2895ade9fdd92b3
Madhushri Deshpande <madhushry12@gmail.com>
2026-07-19 01:44:52 +0530

    Add implementations for Leetcode problems: Linked List Cycle II, Palindrome Linked List, Remove Nth Node From End of List, and Odd Even Linked List

---
+++ Linked List/LLCycleII.cpp
@@ -0,0 +1,35 @@
+//Leetcode 142. Linked List Cycle II
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast){
+                slow = head;
+
+                while(slow != fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+        return NULL;
+    }
+};
---
+++ Linked List/isLLPalindrome.cpp
@@ -0,0 +1,47 @@
+//Leetcode 234. Palindrome Linked List
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverse( ListNode* head){
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* newHead = reverse(head->next);
+        ListNode* front = head->next;
+        front->next = head;
+        head->next = NULL;
+        return newHead;
+    }
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next && fast->next->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* newHead = reverse(slow->next);
+        ListNode * first = head;
+        ListNode* second = newHead;
+
+        while(second != NULL){
+            if(first->val != second->val){
+                reverse(newHead);
+                return false;
+            }
+
+            first = first->next;
+            second = second->next;
+        }
+        reverse(newHead);
+        return true;
+    }
+};
