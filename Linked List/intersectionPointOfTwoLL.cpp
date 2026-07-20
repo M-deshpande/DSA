4a6ac85151598f79a4d43677c4f97cfea818fbbf
Madhushri Deshpande <madhushry12@gmail.com>
2026-07-21 00:36:05 +0530

    Add implementations for Leetcode problems: Delete the Middle Node of a Linked List, Intersection of Two Linked Lists, Rotate List, and Sort List

---
+++ Linked List/intersectionPointOfTwoLL.cpp
@@ -0,0 +1,30 @@
+//Leetcode 160. Intersection of Two Linked Lists
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* t1 = headA;
+        ListNode* t2 = headB;
+
+        while(t1 != t2){
+            t1 = t1->next;
+            t2 = t2->next;
+
+            if(t1 == t2) return t1;
+
+            if(t1 == NULL) t1 = headB;
+            if(t2 == NULL) t2 = headA;
+        }
+        return t1;
+    }
+};
