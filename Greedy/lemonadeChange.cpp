c38f430f5cb4b132d52e67ed0632249a2256021c
Madhushri Deshpande <madhushry12@gmail.com>
2026-07-23 00:41:04 +0530

    Add implementations for Leetcode problems: Assign Cookies, Jump Game, and Lemonade Change

---
+++ Greedy/lemonadeChange.cpp
@@ -0,0 +1,32 @@
+//Leetcode 860. Lemonade Change
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0, ten = 0;
+        for(int i=0; i < bills.size(); i++){
+            if(bills[i] == 5) five++;
+            else if(bills[i] == 10) {
+                if(five){
+                    five --;
+                    ten ++;
+                }
+                else return false;
+            }
+            else {
+                if(ten && five){
+                    ten --;
+                    five --;
+                }
+                else if(five >= 3){
+                    five -= 3;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
+};
